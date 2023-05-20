dd81958a8fdb760c5bd397805eda1e409c95a13c
angelgabrielortegacorzoCAMPUS <angelgabrielorteg@gmail.com>
2023-05-20 17:10:19 -0500

    feat(nuevas interfaces y redireccionamientos): :art: estilos
    se añadio reactividad con js y una interfaz mas agradable
    
    eventos en js

--- apolo.php
+++ apolo.php
@@ -2,16 +2,41 @@
 
 $nombre = $_GET["nombre"];
 $email = $_GET["email"];
-$movil = $_GET["movil"];
+$movil = $_GET["tel"];
 $nivelEstudio = $_GET["nivelEstudio"];
 $lenguajes = explode(',', $_GET["lenguajes"]);
 $ingles = $_GET["ingles"];
 
 
-echo "eres Apolo <br>";
-echo "tu Nombre: " . $nombre . "<br>";
-echo "tu Email: " . $email . "<br>";
-echo "tu Nivel de Estudio: " . $nivelEstudio . "<br>";
-echo "Lenguajes mas usados: " . implode(", ", $lenguajes) . "<br>";
-echo "Nivel de Inglés actual: " . $ingles . "<br>";
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>bienvenido a apolo</title>
+    <link rel="stylesheet" href="redirecciones.css">
+</head>
+<body>
+      <div class="centrar">
+            <?php
+                echo "<div class='dato titulo'>welcome to Apolo</div>";
+                echo "<div class='dato titulo'>en unos dias llegara una respuesta a tu email</div>";
+                echo "<div class='dato'><span class='titulo'>Tu Nombre:</span> " . $nombre . "</div>";
+                echo "<div class='dato'><span class='titulo'>numero:</span> ". $movil. "</div>";
+                echo "<div class='dato'><span class='titulo'>Tu Email:</span> " . $email . "</div>";
+                echo "<div class='dato'><span class='titulo'>Tu Nivel de Estudio:</span> " . $nivelEstudio . "</div>";
+                echo "<div class='dato'><span class='titulo'>Lenguajes más usados:</span> " . implode(", ", $lenguajes) . "</div>";
+                echo "<div class='dato'><span class='titulo'>Nivel de Inglés actual:</span> " . $ingles . "</div>";
+                ?>
+
+                <a href="index.php">volver al inicio</a>
+
+       </div>
+            
+    
+</body>
+</html>
